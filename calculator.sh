8c2bc4bc8e59857872055b126287cb7d3bbf337a
reshma <reshma2004reshu@gmail.com>
2025-03-11 12:28:54 +0530

    product

--- calculator.sh
+++ calculator.sh
@@ -1,17 +1,6 @@
 #!/bin/bash
 
-# Addtion
-x=10
-y=20
-echo "Sum is" $((x+y))
-
-# Subtraction
-a=90
-b=8
-echo "Difference is" $((a-b))
-
 # Multiplication
-
 p=10
 q=10
 echo "Product is" $((p*q))
